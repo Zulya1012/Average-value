7d7d33b7fa094725932adf01373cec4e296606e8
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2023-12-21 21:01:21 +0400

    Update zadacha12.java

--- zadacha12.java
+++ zadacha12.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
-public class zadacha12{
-public static void main(String[] arg){
-int a=12;
-int b=23;
-int c=10;
-Scanner scr=new Scanner(System.in);
-System.out.println((a+b+c)/3);
-}
+public class zadacha12 {
+  public static void main(String[] arg) {
+    int a = 12;
+    int b = 23;
+    int c = 10;
+    Scanner scr = new Scanner(System.in);
+    System.out.println((a + b + c) / 3);
+  }
 }
